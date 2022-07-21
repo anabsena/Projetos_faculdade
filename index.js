0b16e0458963285a5a69117644e7b8ee473f3752
Ana Beatriz <anabsenaf@gmail.com>
2022-07-21 19:22:23 -0300

    delete.js

--- index.js
+++
@@ -1,7 +0,0 @@
-alert("Olá usuário")
-function adicionar(){
-let v=document.getElementById('valor').value;
-let d=document.getElementById('a');
-document.getElementById('espace').innerHTML += "<br><input type='checkbox'>"+v;
-}
-
